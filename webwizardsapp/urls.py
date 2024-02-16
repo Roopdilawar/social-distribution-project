52c76b1609acf3ff05cb6485412e0630fb26ca1e
Neville <nev.albuquerque@gmail.com>
2024-02-16 15:47:27 -0700

    adding posts-api

--- webwizardsapp/urls.py
+++ webwizardsapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import LoginAPIView, RegisterView, AuthorsListView, AuthorDetailView
+from .views import LoginAPIView, RegisterView, AuthorsListView, AuthorDetailView, PostsView, SinglePostView
 
 urlpatterns = [
     path('api/login/', LoginAPIView.as_view(), name='api_login'),
     path('api/signup/', RegisterView.as_view(), name='signup'),
     path('api/authors/', AuthorsListView.as_view(), name='authors-list'),
     path('api/authors/<str:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('api/posts/', PostsView.as_view(), name='posts-list'),
+    path('api/posts/<int:post_id>/', SinglePostView.as_view(), name='single-post'),
 ]
