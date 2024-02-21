2549388b8f616b6ff0d7693c3d2f5a87e5e4b2e7
JuliePilz <jhmpilz@gmail.com>
2024-02-20 17:00:25 -0700

    Implemented routing to followers page

--- webwizards-frontend/src/App.js
+++ webwizards-frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import SignIn from './pages/signin/index.js'; 
 import SignUp from './pages/signup/index.js'; 
 import './App.css';
 import TimelinePage from './pages/timeline';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBell, faHeart, faUser, faComment } from '@fortawesome/free-solid-svg-icons';
+import {faBell, faHeart, faUser, faComment, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+import FollowersPage from './pages/followers/index.js';
 
-function App() {
+function App() {  
+  const navigate = useNavigate();
+
   return (
-    <Router>
       <div className="App">
         <header className="top-nav">
-          <h1 className="header-title">SocialDistribution</h1>
+          <h1 className="header-title" onClick={() => navigate("/") }>SocialDistribution</h1>
           <nav>
             <button className="nav-button">
               <FontAwesomeIcon icon={faBell} />
@@ -20,16 +22,19 @@ function App() {
             <button className="nav-button">
               <FontAwesomeIcon icon={faUser} />
             </button>
+            <button className="nav-button">
+              <FontAwesomeIcon icon={faUserGroup} onClick={() => navigate("/followers")}/>
+            </button>
           </nav>
         </header>
 
         <Routes>
           <Route path="signin" element={<SignIn />} />
           <Route path="signup" element={<SignUp />} />
+          <Route path="followers" element={<FollowersPage />} />
           <Route path="/" element={<TimelinePage />} /> 
         </Routes>
       </div>
-    </Router>
   );
 }
 
---
+++ webwizards-frontend/src/pages/followers/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function FollowersPage (props) {
+
+    return(
+        <div className="followers-page">
+            <h1>
+                Find new followers.
+            </h1>
+        </div>
+    )
+}
