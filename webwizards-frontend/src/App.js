66cc0fd759c37cc0205483096cca69d6a5efb7da
roopdilawar <roopdilawar@gmail.com>
2024-02-24 13:54:27 -0700

    Minor fix for router

--- webwizards-frontend/src/App.js
+++ webwizards-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -11,32 +11,29 @@ import SignIn from './pages/signin/index.js';
 import SignUp from './pages/signup/index.js';
 import TimelinePage from './pages/timeline';
 
-
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              SocialDistribution
-            </Typography>
-            <IconButton color="inherit">
-              <NotificationsIcon />
-            </IconButton>
-            <IconButton color="inherit">
-              <AccountCircle />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SocialDistribution
+          </Typography>
+          <IconButton color="inherit">
+            <NotificationsIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
 
-        <Routes>
-          <Route path="signin" element={<SignIn />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="/" element={<TimelinePage />} />
-        </Routes>
-      </div>
-    </Router>
+      <Routes>
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="/" element={<TimelinePage />} />
+      </Routes>
+    </div>
   );
 }
 
