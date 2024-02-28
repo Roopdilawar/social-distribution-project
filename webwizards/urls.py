de2a7436b39ba9f0f52b91786eda564aa3204fe1
Neville <nev.albuquerque@gmail.com>
2024-02-28 13:31:07 -0700

    Small fix

--- webwizards/urls.py
+++ webwizards/urls.py
@@ -33,7 +33,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("", include("webwizardsapp.urls")),
-    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     
 ]
